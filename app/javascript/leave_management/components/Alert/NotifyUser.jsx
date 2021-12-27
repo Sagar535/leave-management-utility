6d3796824ab27656aa919558db08314c3732a0ca
sagar535 <070bct535@ioe.edu.np>
2021-12-27 13:15:29 +0545

    removes log

--- app/javascript/leave_management/components/Alert/NotifyUser.jsx
+++ app/javascript/leave_management/components/Alert/NotifyUser.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 export default function NotifyUser(message, position, type, notifierRef) {
-  console.log('hi');
   const options = {
     place: position,
     message: (
