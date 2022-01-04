7696b0adf458235b77ea37ea4750c2f0bf50b6f2
sagar535 <070bct535@ioe.edu.np>
2022-01-04 12:18:25 +0545

    adds join date for user

---
+++ db/migrate/20220104062313_add_join_date_to_user.rb
@@ -0,0 +1,5 @@
+class AddJoinDateToUser < ActiveRecord::Migration[6.1]
+  def change
+    add_column :users, :join_date, :date
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,11 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-admin = User.create({first_name: 'Subas', last_name: 'Poudel', email: 'dit.subas@gmail.com', password: 'foobar', role: 'admin'})
+today = Time.zone.today
+admin = User.create({first_name: 'Subas', last_name: 'Poudel', email: 'dit.subas@gmail.com', password: 'foobar', role: 'admin', join_date: today})
 users = User.create([
-    {first_name: 'Sagar', last_name: 'Shah', email: 'dit.sagar@gmail.com', password: 'foobar', role: 'user'},
-    {first_name: 'Saga', last_name: 'Shah', email: 'dit.saga@gmail.com', password: 'foobar', role: 'user'},
-    {first_name: 'Sag', last_name: 'Shah', email: 'dit.sag@gmail.com', password: 'foobar', role: 'user'},
-    {first_name: 'Sa', last_name: 'Shah', email: 'dit.sa@gmail.com', password: 'foobar', role: 'user'},
+    {first_name: 'Sagar', last_name: 'Shah', email: 'dit.sagar@gmail.com', password: 'foobar', role: 'user', join_date: today},
+    {first_name: 'Saga', last_name: 'Shah', email: 'dit.saga@gmail.com', password: 'foobar', role: 'user',join_date: today},
+    {first_name: 'Sag', last_name: 'Shah', email: 'dit.sag@gmail.com', password: 'foobar', role: 'user',join_date: today},
+    {first_name: 'Sa', last_name: 'Shah', email: 'dit.sa@gmail.com', password: 'foobar', role: 'user',join_date: today},
 ])
