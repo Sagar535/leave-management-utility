181018dc2d4d4336cce2c839e5f2eb4f7eca9327
sagar535 <070bct535@ioe.edu.np>
2022-01-11 13:45:18 +0545

    sends dynamic path to leave management component

--- app/javascript/packs/App.jsx
+++ app/javascript/packs/App.jsx
@@ -5,9 +5,11 @@ import LeaveManagementApp from '../leave_management';
 document.addEventListener('DOMContentLoaded', () => {
   const node = document.getElementById('user_data');
   const data = JSON.parse(node.getAttribute('data')) || {};
+  const path_node = document.getElementById('path_data');
+  const path_data = path_node.getAttribute('data') || '/admin/dashboard'
 
   ReactDOM.render(
-    <LeaveManagementApp data={data} />,
+    <LeaveManagementApp data={data} path={path_data}/>,
     document.body.appendChild(document.createElement('div')),
   );
 });
