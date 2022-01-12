1f5c455c498b0b5e1e180e0a8a513282e74a39f0
sagar535 <070bct535@ioe.edu.np>
2022-01-12 12:12:25 +0545

    adds leave balances to user serializers

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,5 @@
 class UserSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :id, :email, :role, :first_name, :last_name, :join_date, :leave_request_days_count, :sick_leave_balance
+  attributes :id, :email, :role, :first_name, :last_name, :join_date,
+             :leave_request_days_count, :sick_leave_balance, :paid_leave_balance, :unpaid_leave_balance
 end
