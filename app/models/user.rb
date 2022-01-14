6051691b4e96984310483be157a72e2ed465107d
sagar535 <070bct535@ioe.edu.np>
2022-01-14 13:09:45 +0545

    pronto fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,9 @@ class User < ApplicationRecord
   enum role: { user: 0, admin: 1 }
   has_many :leave_requests
   has_many :approved_leave_requests, class_name: 'LeaveRequest', foreign_key: :approver_id, dependent: :nullify, inverse_of: :approver
-  has_many :salaries
+  has_many :salaries, dependent: :destroy
 
-  has_one :active_salary, -> { where(active: true) }, class_name: 'Salary'
+  has_one :active_salary, -> { where(active: true) }, class_name: 'Salary', dependent: :destroy, inverse_of: :user
 
   validates :first_name, :last_name, presence: true
 
