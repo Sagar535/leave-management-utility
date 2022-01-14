b9b8a505d52e365ccf351d415593d534fb68df13
sagar535 <070bct535@ioe.edu.np>
2022-01-14 17:38:14 +0545

    json endpoint for a single salary_setting

--- app/controllers/salary_settings_controller.rb
+++ app/controllers/salary_settings_controller.rb
@@ -1,9 +1,21 @@
 class SalarySettingsController < ApplicationController
   before_action -> { authorize_class(User) }, only: %i[index]
+  before_action :set_salary_setting, only: [:show]
   skip_before_action :verify_authenticity_token
 
   def index
     @salary_settings = SalarySetting.all
     render json: SalariesSettingSerializer.new(@salary_settings).serialized_json
+  end
+
+  def show
+    render json: SalariesSettingSerializer.new(@salary_setting).serialized_json
+  end
+
+  private
+
+  def set_salary_setting
+    @salary_setting = SalarySetting.find(params[:id])
+    authorize @salary_setting
   end
 end
