43b4b73561ea3d5fea21a67f358d981a5353c6cc
sagar535 <070bct535@ioe.edu.np>
2022-01-14 12:34:15 +0545

    makes sure to_date is present when slary is made inactive

--- app/models/salary.rb
+++ app/models/salary.rb
@@ -3,5 +3,6 @@ class Salary < ApplicationRecord
   has_many :tax_rules
 
   validates :basic_salary, :commitment_bonus, :from_date, presence: true
+  validates :to_date, presence: true, unless: :active?
   validates :active, uniqueness: { scope: :user }, if: :active
 end
