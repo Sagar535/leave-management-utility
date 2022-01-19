57015f36e31db3fc1c43dd64ee7175e995ce46c3
sagar535 <070bct535@ioe.edu.np>
2022-01-19 16:43:29 +0545

    fetches users and tax rules as well, while fetching salary setting

--- app/serializers/salaries_setting_serializer.rb
+++ app/serializers/salaries_setting_serializer.rb
@@ -1,4 +1,6 @@
 class SalariesSettingSerializer
   include FastJsonapi::ObjectSerializer
   attributes :id, :ssf_office, :ssf_employee, :life_insurance_max, :ssf_tax_exemption_rate, :ssf_tax_exemption_max
+  has_many :users
+  has_many :tax_rules
 end
