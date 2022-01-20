5561d0de596459bd581a6693736fe2ccd43552dd
sagar535 <070bct535@ioe.edu.np>
2022-01-20 12:11:22 +0545

    lint fixes

--- app/javascript/leave_management/components/Forms/TaxRuleForm.jsx
+++ app/javascript/leave_management/components/Forms/TaxRuleForm.jsx
@@ -1,44 +1,47 @@
 import React from 'react';
-import {Button, Form, FormGroup, Input, Label} from "reactstrap";
+import {
+  Button, Form, FormGroup, Input, Label,
+} from 'reactstrap';
 
 export default function TaxRuleForm(props) {
-    return (
-        <>
-            <Form
-                onSubmit={props.handleSubmit}
-                data-remote={true}
-            >
-                <FormGroup>
-                    <Label for='name'>Tax Name</Label>
-                    <Input id='name'
-                           placeholder='Tax Name'
-                           required
-                           value={props.taxRule.name || ''}
-                           onChange={(e) => props.setTaxRule({...props.taxRule, name: e.target.value})}
-                    />
-                </FormGroup>
-                <FormGroup>
-                    <Label for='from_date'>From Date</Label>
-                    <Input id='from_date'
-                           placeholder='Begin Date for Tax Rule'
-                           type='date'
-                           required
-                           value={props.taxRule.from_date || ''}
-                           onChange={(e) => props.setTaxRule({...props.taxRule, from_date: e.target.value})}
-                    />
-                </FormGroup>
-                <FormGroup>
-                    <Label for='to_date'>To Date</Label>
-                    <Input id='to_date'
-                           placeholder='End Date for Tax Rule'
-                           required
-                           type='date'
-                           value={props.taxRule.to_date || ''}
-                           onChange={(e) => props.setTaxRule({...props.taxRule, to_date: e.target.value})}
-                    />
-                </FormGroup>
-                <Button color='success' type='Submit'>Submit</Button>
-            </Form>
-        </>
-    )
+  return (
+    <Form
+      onSubmit={props.handleSubmit}
+      data-remote
+    >
+      <FormGroup>
+        <Label for="name">Tax Name</Label>
+        <Input
+          id="name"
+          placeholder="Tax Name"
+          required
+          value={props.taxRule.name || ''}
+          onChange={(e) => props.setTaxRule({ ...props.taxRule, name: e.target.value })}
+        />
+      </FormGroup>
+      <FormGroup>
+        <Label for="from_date">From Date</Label>
+        <Input
+          id="from_date"
+          placeholder="Begin Date for Tax Rule"
+          type="date"
+          required
+          value={props.taxRule.from_date || ''}
+          onChange={(e) => props.setTaxRule({ ...props.taxRule, from_date: e.target.value })}
+        />
+      </FormGroup>
+      <FormGroup>
+        <Label for="to_date">To Date</Label>
+        <Input
+          id="to_date"
+          placeholder="End Date for Tax Rule"
+          required
+          type="date"
+          value={props.taxRule.to_date || ''}
+          onChange={(e) => props.setTaxRule({ ...props.taxRule, to_date: e.target.value })}
+        />
+      </FormGroup>
+      <Button color="success" type="Submit">Submit</Button>
+    </Form>
+  );
 }
--- app/javascript/leave_management/views/taxRule/TaxRuleDashboard.jsx
+++ app/javascript/leave_management/views/taxRule/TaxRuleDashboard.jsx
@@ -1,177 +1,178 @@
 import {
-    Button,
-    Card, CardHeader, CardBody,
-    Col, Nav, Row, Modal
-} from "reactstrap";
-import React, {useEffect, useState} from "react";
-import ReactTable from "../../components/ReactTable/ReactTable";
-import apiCall from "../../helpers/apiCall";
-import Jsona from "jsona";
-import TaxRuleForm from "../../components/Forms/TaxRuleForm";
+  Button,
+  Card, CardHeader, CardBody,
+  Col, Nav, Row, Modal,
+} from 'reactstrap';
+import React, { useEffect, useState } from 'react';
+import Jsona from 'jsona';
+import ReactTable from '../../components/ReactTable/ReactTable';
+import apiCall from '../../helpers/apiCall';
+import TaxRuleForm from '../../components/Forms/TaxRuleForm';
 
 export default function TaxRuleDashboard(props) {
-    const [createTaxRule, setCreateTaxRule] = useState(false)
-    const [updateTaxRule, setUpdateTaxRule] = useState(false)
-    const [taxRule, setTaxRule] = useState({})
-    const [taxRules, setTaxRules] = useState([])
-    const [taxRuleId, setTaxRuleId] = useState()
+  const [createTaxRule, setCreateTaxRule] = useState(false);
+  const [updateTaxRule, setUpdateTaxRule] = useState(false);
+  const [taxRule, setTaxRule] = useState({});
+  const [taxRules, setTaxRules] = useState([]);
+  const [taxRuleId, setTaxRuleId] = useState();
 
-    useEffect(() => {
-        apiCall.fetchEntities('/tax_rules.json')
-            .then((res) => {
-                const dataFormatter = new Jsona();
-                setTaxRules(dataFormatter.deserialize(res.data))
-            })
-    }, [])
+  useEffect(() => {
+    apiCall.fetchEntities('/tax_rules.json')
+      .then((res) => {
+        const dataFormatter = new Jsona();
+        setTaxRules(dataFormatter.deserialize(res.data));
+      });
+  }, []);
 
-    const onRowClick = (state, rowInfo) => ({
-        onClick: (e) => {
-            setUpdateTaxRule(true)
-            console.log(rowInfo.original)
-            setTaxRuleId(rowInfo.original.id)
-            setTaxRule(rowInfo.original)
-        }
-    })
+  const onRowClick = (state, rowInfo) => ({
+    onClick: (e) => {
+      setUpdateTaxRule(true);
+      console.log(rowInfo.original);
+      setTaxRuleId(rowInfo.original.id);
+      setTaxRule(rowInfo.original);
+    },
+  });
 
-    const create = () => {
-        apiCall.submitEntity({tax_rule: taxRule}, `/tax_rules.json`)
-            .then((res) => {
-                const dataFormatter = new Jsona()
-                const taxRule = dataFormatter.deserialize(res.data)
-                setTaxRules([...taxRules, taxRule])
-            })
-    }
+  const create = () => {
+    apiCall.submitEntity({ tax_rule: taxRule }, '/tax_rules.json')
+      .then((res) => {
+        const dataFormatter = new Jsona();
+        const taxRule = dataFormatter.deserialize(res.data);
+        setTaxRules([...taxRules, taxRule]);
+      });
+  };
 
-    const update = () => {
-        apiCall.submitEntity({tax_rule: taxRule}, `/tax_rules/${taxRuleId}.json`, 'PATCH')
-            .then((res) => {
-                const dataFormatter = new Jsona()
-                const updatedTaxRule = dataFormatter.deserialize(res.data)
-                const taxRuleIndex = taxRules.findIndex((taxRule) => taxRule.id === updatedTaxRule.id)
-                taxRules.splice(taxRuleIndex, 1, updatedTaxRule)
-                setTaxRules(taxRules)
+  const update = () => {
+    apiCall.submitEntity({ tax_rule: taxRule }, `/tax_rules/${taxRuleId}.json`, 'PATCH')
+      .then((res) => {
+        const dataFormatter = new Jsona();
+        const updatedTaxRule = dataFormatter.deserialize(res.data);
+        const taxRuleIndex = taxRules.findIndex((taxRule) => taxRule.id === updatedTaxRule.id);
+        taxRules.splice(taxRuleIndex, 1, updatedTaxRule);
+        setTaxRules(taxRules);
 
-                // close the modal form
-                setUpdateTaxRule(false)
-            })
-            .catch((e) => {
-                console.log(e)
-            })
-    }
+        // close the modal form
+        setUpdateTaxRule(false);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
 
-    return (
-        <>
-            <Card className="shadow mb-0">
-                <CardHeader className="border-0 text-white bg-primary pb-6 px-5">
-                    <Row className="pt-4">
-                        <Col lg="6">
-                            <h6 className="fullcalendar-title h2 text-white d-inline-block mb-0 mr-1">
-                                Tax Rule Dashboard
-                            </h6>
-                            <Nav aria-label="breadcrumb" className="d-none d-inline-block ml-lg-4">
-                                <ol className="breadcrumb breadcrumb-links breadcrumb-dark" style={{backgroundColor: 'inherit'}}>
-                                    <li className="breadcrumb-item"><i className="fas fa-home" /></li>
-                                    <li className="breadcrumb-item" aria-current="page">Dashboard</li>
-                                    <li className="breadcrumb-item" onClick={() => props.history.push('/admin/calendar')}>Calendar</li>
-                                    <li className="breadcrumb-item active" onClick={() => props.history.push('/admin/admin')}>Admin</li>
-                                </ol>
-                            </Nav>
-                        </Col>
-                    </Row>
-                </CardHeader>
-                <CardBody className="mt--6">
-                    <div className="bg-white shadow-lg p-5 pb-7" style={{ borderRadius: 5 }}>
-                        <Button
-                            color='success'
-                            size='sm'
-                            className='mb-2'
-                            onClick={() => {
-                                setCreateTaxRule(true)
-                                setTaxRule({})
-                            }}>
-                            New Tax Rule
-                        </Button>
-                        <ReactTable
-                            resizable={false}
-                            data={taxRules}
-                            loading={false}
-                            columns={[
-                                {
-                                    Header: 'Name',
-                                    accessor: 'name',
-                                    style: { whiteSpace: 'unset' },
-                                },
-                                {
-                                    Header: 'From Date',
-                                    accessor: 'from_date',
-                                    style: {whiteSpace: 'unset'}
-                                },
-                                {
-                                    Header: 'To Date',
-                                    accessor: 'to_date',
-                                    style: {whiteSpace: 'unset'}
-                                },
-                            ]}
-                            defaultPageSize={5}
-                            showPaginationBottom
-                            className="-striped -highlight text-capitalize"
-                            getTrProps={onRowClick}
-                        />
-                    </div>
-                </CardBody>
-            </Card>
-
-            <Modal
-                isOpen={createTaxRule}
-                toggle={() => setCreateTaxRule(false)}
-                className="modal-dialog-centered modal-secondary"
+  return (
+    <>
+      <Card className="shadow mb-0">
+        <CardHeader className="border-0 text-white bg-primary pb-6 px-5">
+          <Row className="pt-4">
+            <Col lg="6">
+              <h6 className="fullcalendar-title h2 text-white d-inline-block mb-0 mr-1">
+                Tax Rule Dashboard
+              </h6>
+              <Nav aria-label="breadcrumb" className="d-none d-inline-block ml-lg-4">
+                <ol className="breadcrumb breadcrumb-links breadcrumb-dark" style={{ backgroundColor: 'inherit' }}>
+                  <li className="breadcrumb-item"><i className="fas fa-home" /></li>
+                  <li className="breadcrumb-item" aria-current="page">Dashboard</li>
+                  <li className="breadcrumb-item" onClick={() => props.history.push('/admin/calendar')}>Calendar</li>
+                  <li className="breadcrumb-item active" onClick={() => props.history.push('/admin/admin')}>Admin</li>
+                </ol>
+              </Nav>
+            </Col>
+          </Row>
+        </CardHeader>
+        <CardBody className="mt--6">
+          <div className="bg-white shadow-lg p-5 pb-7" style={{ borderRadius: 5 }}>
+            <Button
+              color="success"
+              size="sm"
+              className="mb-2"
+              onClick={() => {
+                setCreateTaxRule(true);
+                setTaxRule({});
+              }}
             >
-                <div className="modal-header p-1">
-                    <button
-                        aria-hidden
-                        className="close"
-                        data-dismiss="modal"
-                        type="button"
-                        onClick={() => setCreateTaxRule(false)}
-                    >
-                        <i className="tim-icons icon-simple-remove" />
-                    </button>
-                </div>
+              New Tax Rule
+            </Button>
+            <ReactTable
+              resizable={false}
+              data={taxRules}
+              loading={false}
+              columns={[
+                {
+                  Header: 'Name',
+                  accessor: 'name',
+                  style: { whiteSpace: 'unset' },
+                },
+                {
+                  Header: 'From Date',
+                  accessor: 'from_date',
+                  style: { whiteSpace: 'unset' },
+                },
+                {
+                  Header: 'To Date',
+                  accessor: 'to_date',
+                  style: { whiteSpace: 'unset' },
+                },
+              ]}
+              defaultPageSize={5}
+              showPaginationBottom
+              className="-striped -highlight text-capitalize"
+              getTrProps={onRowClick}
+            />
+          </div>
+        </CardBody>
+      </Card>
 
-                <div className="modal-body">
-                    <TaxRuleForm
-                        taxRule={taxRule}
-                        setTaxRule={setTaxRule}
-                        handleSubmit={create}
-                    />
-                </div>
-            </Modal>
+      <Modal
+        isOpen={createTaxRule}
+        toggle={() => setCreateTaxRule(false)}
+        className="modal-dialog-centered modal-secondary"
+      >
+        <div className="modal-header p-1">
+          <button
+            aria-hidden
+            className="close"
+            data-dismiss="modal"
+            type="button"
+            onClick={() => setCreateTaxRule(false)}
+          >
+            <i className="tim-icons icon-simple-remove" />
+          </button>
+        </div>
 
-            <Modal
-                isOpen={updateTaxRule}
-                toggle={() => setUpdateTaxRule(false)}
-                className="modal-dialog-centered modal-secondary"
-            >
-                <div className="modal-header p-1">
-                    <button
-                        aria-hidden
-                        className="close"
-                        data-dismiss="modal"
-                        type="button"
-                        onClick={() => setUpdateTaxRule(false)}
-                    >
-                        <i className="tim-icons icon-simple-remove" />
-                    </button>
-                </div>
+        <div className="modal-body">
+          <TaxRuleForm
+            taxRule={taxRule}
+            setTaxRule={setTaxRule}
+            handleSubmit={create}
+          />
+        </div>
+      </Modal>
 
-                <div className="modal-body">
-                    <TaxRuleForm
-                        taxRule={taxRule}
-                        setTaxRule={setTaxRule}
-                        handleSubmit={update}
-                    />
-                </div>
-            </Modal>
-        </>
-    )
+      <Modal
+        isOpen={updateTaxRule}
+        toggle={() => setUpdateTaxRule(false)}
+        className="modal-dialog-centered modal-secondary"
+      >
+        <div className="modal-header p-1">
+          <button
+            aria-hidden
+            className="close"
+            data-dismiss="modal"
+            type="button"
+            onClick={() => setUpdateTaxRule(false)}
+          >
+            <i className="tim-icons icon-simple-remove" />
+          </button>
+        </div>
+
+        <div className="modal-body">
+          <TaxRuleForm
+            taxRule={taxRule}
+            setTaxRule={setTaxRule}
+            handleSubmit={update}
+          />
+        </div>
+      </Modal>
+    </>
+  );
 }
