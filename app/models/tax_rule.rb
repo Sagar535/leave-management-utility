af93c4761d6a087d69890f625a98bc0826fd1ae7
sagar535 <070bct535@ioe.edu.np>
2022-02-21 13:59:18 +0545

    amount validation in tax rules

--- app/models/tax_rule.rb
+++ app/models/tax_rule.rb
@@ -2,4 +2,11 @@ class TaxRule < ApplicationRecord
   belongs_to :salary_setting, optional: true
 
   validates :amount_to, :amount_from, :rate, presence: true
+  validate :amount_set_properly
+
+  private
+
+  def amount_set_properly
+    self.errors.add :message, "Amount to must be greater than amount from..." if amount_from > amount_to
+  end
 end
